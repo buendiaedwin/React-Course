b6a9731e6327ba052a90b41b92284ebf7142b55f
Edwin Buendia <buendiaedwin2@gmail.com>
2025-02-19 22:15:13 -0600

    First Commit
    Created course React project and new components like Header etc.

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { CORE_CONCEPTS } from "./data";
+import Header from "./components/Header"
+import CoreConcept from "./components/CoreConcept";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+        <h2>Time to get started!</h2>
+        <ul>
+          <CoreConcept {...CORE_CONCEPTS[0]}/>
+          <CoreConcept {...CORE_CONCEPTS[1]}/>
+          <CoreConcept {...CORE_CONCEPTS[2]}/>
+          <CoreConcept 
+          title={CORE_CONCEPTS[3].title} 
+          description={CORE_CONCEPTS[3].description}
+          image={CORE_CONCEPTS[3].image}/>
+        </ul>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
